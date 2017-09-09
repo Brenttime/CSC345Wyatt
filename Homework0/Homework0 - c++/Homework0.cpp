e08c1819b9d2a955622e823b86bbe16cf706836e
Brenttime <l7gaming@yahoo.com>
2017-09-09 14:40:20 -0400

    Output files added

--- Homework0/Homework0 - c++/Homework0.cpp
+++ Homework0/Homework0 - c++/Homework0.cpp
@@ -1,6 +1,7 @@
 /*
 	CSC345 - Homework 0 - C++
 	By: Brent Turner
+	09/02/17
 */
 #include "stdafx.h"
 #include <iostream>
@@ -17,21 +18,23 @@ int main()
 	int y = square(x) + twice(x) + once(x); //y == 45, not 18
 	cout << y << endl; //prints 45 not 18
 	_sleep(1000);
-    return 0;
+	return 0;
 }
 
+/*Square function x * x */
 int square(int &x)
 {
 	x = x * x;
 	return x;
 }
-
+/*2 * x function */
 int twice(int &x)
 {
 	x = x * 2;
 	return x;
 }
 
+/*just x function*/
 int once(int &x)
 {
 	return x;
