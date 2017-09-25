5651a4849061deaa529d96245b4804ba645fac8d
Brent A. Turner <brentturner7@gmail.com>
2017-09-25 18:22:36 -0400

    timings added

--- Homework 1/assignment1.lisp
+++ Homework 1/assignment1.lisp
@@ -41,18 +41,45 @@
     (fibonacci-aux 1 0 n)))
 
 
-;;These below functions start the fibonacci sequence at 1 rather than at 0 like above
 
-;;(defun fibonacci (n)
-  ;;"This will implement the stupid double recursion of Fibonacci numbers"
-  ;;(cond ((eql n 1) 1)
-;;	((eql n 2) 1)
-;;	((+ (fibonacci (- n 1)) (fibonacci (- n 2))))))
-  
-;;(defun fibonacci-TR (n)
-  ;;"This will implement the smarter tail recursion of fibonacci  numbers"
-  ;;(defun fib-aux (oneBehind twoBehind current)
-    ;;(if (= current n)
-	;;(+ oneBehind twoBehind)
-	;;(fib-aux (+ oneBehind twoBehind) oneBehind (+ 1 current))))
-  ;;(if (< n 3) 1 (fib-aux 1 1 3)))
+
+
+
+;;; FOR USE IN HOMEWORK 1
+
+;;;==================================================================================
+;;; a macro to do FOR loops  ;; equivalent to Java for-loop control: for (int var=start, var<=stop; var+=update) body
+(defmacro for ((var start stop update) &body body)
+  (let ((gstop (gensym))                    ;; generate new symbols, GUARANTEED to be new; prevents capture
+	(gupdate (gensym)))
+    `(do ( (,gupdate ,update)               ;; needed so that the update expression is evaluated just once
+	   (,var ,start (+ ,gupdate ,var))  ;; needed so that the stop expression is evaluated just once
+	   (,gstop ,stop))
+         ((> ,var ,gstop))
+       ,@body)))
+
+
+;; EXAMPLE
+;; CL-USER> (for (i 1 6 1) (print i))     ;; equivalent to Java for-loop control: (for int i=1, i<=6; i++)
+;;
+;; 1 
+;; 2 
+;; 3 
+;; 4 
+;; 5 
+;; 6 
+;; NIL
+
+;;;==================================================================================
+(defun comparefibonaccis()
+  (for (i 10 35 5)                               ;; for (int i=10; i<=35, i+=5) ...
+       (format t "TAIL REC FIBONACCI ~a~%" i)
+       (time(fibonacci-TR i))
+       (format t "FIBONACCI ~a~%" i)
+       (time(fibonacci i))
+       (format t "=======================================================~%")
+       )
+  )
+
+;;;==================================================================================
+;;; END 
