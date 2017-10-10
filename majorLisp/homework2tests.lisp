34e22be98992d0704b2d76e72f3ce5195143cde4
Brent A. Turner <brentturner7@gmail.com>
2017-10-10 14:50:57 -0400

    Working copy

--- majorLisp/homework2tests.lisp
+++ majorLisp/homework2tests.lisp
@@ -12,22 +12,32 @@
 (defun differentiate (F V)
   (cond ((constant-p F) (make-constant 0)) ;;if F is a constant eval 0
 
-	((sum-p F) (make-sum (differentiate (sum-operand-1 F) V) (differentiate (sum-operand-2 F) V)))
-	
-;;;	((negation-p F) (make-negation (differentiate F V))) ;;in the case of negation
-	((negation-p F) (make-negation (differentiate (rest F) V)))
-	
-        ((subtraction-p F) (make-subtraction
-			    (differentiate (subtraction-operand-1 F ) V) (differentiate (sum-operand-2 F) V)))
-	((product-p F) (make-product (differentiate (product-operand-1 F) V) (differentiate (sum-operand-2 F) V)))
-	((division-p F) (make-division (differentiate (division-operand-1 F) V) (differentiate (division-operand-2 F) V)))
-	((power-p F) (make-power (differentiate (power-operand-1 F) V) (power-operand-2 F)))
+	((sum-p F)
+	 (make-sum (differentiate (sum-operand-1 F) V) (differentiate (sum-operand-2 F) V)))
+
+	((negation-p F)
+	 (make-negation (differentiate (rest F) V)))
+
+	((subtraction-p F)
+	 (make-subtraction (differentiate (subtraction-operand-1 F ) V) (differentiate (sum-operand-2 F) V)))
+
+	((product-p F)
+	 (make-product (differentiate (product-operand-1 F) V) (differentiate (sum-operand-2 F) V)))
+
+	((division-p F)
+	 (make-division (differentiate (division-operand-1 F) V) (differentiate (division-operand-2 F) V)))
+
+	((power-p F)
+	 (make-power (differentiate (power-operand-1 F) V) (power-operand-2 F)))
+
 	((variable-p F) (if (equal (make-variable F) (make-variable V))
 			    (make-constant 1)
 			    (make-constant 0)))))
 
 (defun make-constant (C) C)
-(defun make-variable (V) V)
+(defun make-variable (V)
+  (cond ((listp V) (list (first V)))
+	(t(list V))))
 
 ;;predicates
 (defun constant-p (F)
@@ -35,27 +45,32 @@
 
 (defun variable-p (V)
   (cond ((member V variable-symbols) t)
-	((and (listp V) (variable-p (first V))) (member (first V) variable-symbols))))
-
- ;;; (member V variable-symbols))
+	((and (listp V) (variable-p (first V)))
+	 (member (first V) variable-symbols))))
 
 (defun negation-p (F)
-  (and (listp F) (equal (first F) negation-symbol)))
+  (and (listp F)
+       (equal (first F) negation-symbol)))
 
 (defun sum-p (F)
-  (and (listp F) (equal (sum-operator F) sum-symbol) (>= (length F) 3)))
+  (and (listp F)
+       (equal (sum-operator F) sum-symbol) (>= (length F) 3)))
 
 (defun subtraction-p (F)
-  (and (listp F) (equal (subtraction-operator F) subtraction-symbol) (>= (length F) 3)))
+  (and (listp F)
+       (equal (subtraction-operator F) subtraction-symbol) (>= (length F) 3)))
 
 (defun product-p (F)
-  (and (listp F) (equal (product-operator F) product-symbol) (>= (length F) 3)))
+  (and (listp F)
+       (equal (product-operator F) product-symbol) (>= (length F) 3)))
 
 (defun division-p (F)
-  (and (listp F) (equal (division-operator F) division-symbol) (>= (length F) 3)))
+  (and (listp F)
+       (equal (division-operator F) division-symbol) (>= (length F) 3)))
 
 (defun power-p (F)
-  (and (listp F) (equal (power-operator F) power-symbol) (>= (length F) 3)))
+  (and (listp F)
+       (equal (power-operator F) power-symbol) (>= (length F) 3)))
 
 ;;operators
 (defun negation-operator (F) (first F))
@@ -109,6 +124,7 @@
 (defun make-power (F G)
   (cond ((eq F 0) 0)
 	((eq G 0) 1)
-	((and (numberp F) (numberp G)) (* F (make-power F (- G 1)))) ;;;subtract 1 from G for reccursion
+	((and (numberp F) (numberp G))
+	 (* F (make-power F (- G 1)))) ;;;subtract 1 from G for reccursion
 	(t(list F power-symbol G))))
 
